51bcc06be1f75a396fbf114824491e26080965a4
Sergei Alekseev <alekseevsa@tutu.ru>
2020-06-09 22:14:22 +0300

    Async demo

--- replay_subject/index.js
+++ replay_subject/index.js
@@ -1,4 +1,6 @@
-import { ReplaySubject } from 'rxjs';
+import { ReplaySubject, race } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { tap } from 'rxjs/operators';
 
 const subject = new ReplaySubject(2);
 
@@ -18,14 +20,26 @@ subject.next(5);
 
 console.clear();
 
-const subject2 = new ReplaySubject(30, 200 /* 500 */);
+// const subject2 = new ReplaySubject(30, 200 /* 500 */);
 
-subject2.subscribe(v => console.log(`observer 3: ${v}`))
+// subject2.subscribe(v => console.log(`observer 3: ${v}`))
 
-let i = 1;
-const interval = setInterval(() => subject2.next(i++), 100);
+// let i = 1;
+// const interval = setInterval(() => subject2.next(i++), 100);
 
-setTimeout(() => {
-  subject2.subscribe((v) => console.log(`observer 4: ${v}`));
-  clearInterval(interval);
-}, 500);
+// setTimeout(() => {
+//   subject2.subscribe((v) => console.log(`observer 4: ${v}`));
+//   clearInterval(interval);
+// }, 500);
+
+const cached = new ReplaySubject(1, 3000);
+
+const req = ajax('https://jsonplaceholder.typicode.com/todos/1').pipe(
+  tap((data) => {
+    cached.next(data);
+  })
+);
+
+setInterval(() => {
+  race(cached, req).subscribe(v => console.log(v));
+}, 300);
