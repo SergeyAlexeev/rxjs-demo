0d767b7c1cfa78985eb9f2ed4e1283c31367391a
SergeyAlexeev <SergeyAlexeev@users.noreply.github.com>
2020-06-10 21:37:49 +0300

    Clean up

--- subject/index.js
+++ subject/index.js
@@ -11,14 +11,3 @@ subject.subscribe(v=> console.log(`observer 2: ${v}`));
 
 subject.next(3);
 subject.next(4);
-
-// Make Observable multicasted
-
-// const subject2 = new Subject();
-
-// subject2.subscribe(v => console.log(`observer 3: ${v}`));
-// subject2.subscribe(v => console.log(`observer 4: ${v}`));
-
-// const observable = from(['a', 'b', 'c']);
-
-// observable.subscribe(subject2);
